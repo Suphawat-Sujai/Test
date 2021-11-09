dcc345ef4894c444d819b554dcf1b0e0e59d551d
Suphawat-Sujai <suphawat_2544_196@hotmail.com>
2021-11-09 19:42:47 +0700

    test
    create

--- rt.cpp
+++ rt.cpp
@@ -1,2 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hellow world" << endl; //<< endl; is newline
+    cout << "I love yamnit"<< "\n";
+
+    int number = 10;
+    int m = 30;
+    cout << number + m << "\n"; // cout เหมือนเป็นการเเสดงผล
     
+    float x = 3.54;
+    float y = 22.11;
+    cout << x+y <<  "\n";
+
+    double e;
+    cout << "Number is ";
+    cin >> e; //cin เหมือนเป็นตัวรับค่าทางคีย์บอร์ด 
     cout << "answer is " << e;
+    cout << endl;
+
+    string name;
+    cout << "Type your ftirst name ";
+    cin >> name;
+    cout << "Your name is " << name;
+    
+    
+    return 0; //คล้ายเป็นตัวจบการทำงาน
+}
